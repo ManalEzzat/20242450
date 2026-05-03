fb1fd5f0e7d72bba24d80b4b89b72a293f5f3251
ManalEzzat <manalezzat590@gmail.com>
2026-05-04 01:10:17 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <string>
+#include "Employee.h"
+#include "EmployeeController.h"
+
 using namespace std;
-int main(){
-  
+
+int main() {
+    Employee* model = new Employee(1, "manal ezzat", "manal@email.com");
+
+    EmployeeView* view = new EmployeeView();
+
+    EmployeeController controller(model, view);
+
+    cout << "--- Current Employee Data ---" << endl;
+    controller.updateView();
+
+    controller.setEmployeeName("ahmed ezzat");
+    controller.setEmployeeEmail("ahmed@email.com");
+    controller.setEmployeeId(2024);
+
+    cout << "\n--- Updated Employee Data ---" << endl;
+    controller.updateView();
+
+    delete model;
+    delete view;
+
+    return 0;
 }
